01a4de0e47ed59161b849c62daf4b1be3e8cb1b3
Olamide1 <olamideakomolafe1234@gmail.com>
2020-02-10 23:39:53 +0300

    Bootstrap added in setup

--- managedby-frontend/managedby/src/main.js
+++ managedby-frontend/managedby/src/main.js
@@ -3,7 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import 'bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
